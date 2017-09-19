f31dccfd0fe68099680f562592bb944371765b0d
Jordi <me@jordify.nl>
2017-09-19 19:37:32 +0200

    Initial commit of airports app

---
+++ Airports/app/src/main/java/pw/jordi/airports/AirportListActivity.java
@@ -0,0 +1,13 @@
+package pw.jordi.airports;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class AirportListActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_airport_list);
+    }
+}
